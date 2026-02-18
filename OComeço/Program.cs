90566bd0b1b9f3f7dec8807cd05f8c38c2125a08
Diogo de Alcantara Brasil <diiogobrasill@gmail.com>
2026-02-18 18:21:38 -0300

    Inicializa projeto de chamados de TI

---
+++ OComeço/Program.cs
@@ -0,0 +1,44 @@
+﻿using OComeço;
+
+GerenciadorChamados gerenciador = new();
+while (true)
+{
+
+
+    Console.WriteLine("\nMenu\t");
+
+    Console.WriteLine("Escolha qual opção voce deseja:\n 1 = Criar Chamado\n 2 = Listar Chamados\n 3 = Fechar Chamado\n Digite 0 para sair do programa\n");
+    int resposta = Convert.ToInt32(Console.ReadLine());
+
+    if (resposta == 0)
+    {
+        break;
+    }
+
+    if (resposta == 1)
+    {
+        Console.WriteLine("Digite o título");
+        string? titulo = Console.ReadLine();
+
+        Console.WriteLine("Digite a descrição");
+        string? descricao = Console.ReadLine();
+
+        gerenciador.Criar(titulo, descricao);
+    }
+    else if (resposta == 2)
+    {
+        gerenciador.Listar();
+    }
+    else if (resposta == 3)
+    {
+        gerenciador.Fechar();
+    }
+    else
+    {
+        Console.WriteLine("Opção invalida");
+    }
+
+
+}
+
+    Console.ReadKey();
